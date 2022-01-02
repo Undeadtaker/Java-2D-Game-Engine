4f345986dd853a29df1bdcd19ad6d2e50304456b
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-02 17:01:50 +0100

    Update Shader.java
    The Shader now has a new function uploadMat4f which now takes the Matrix4f, stores it into a buffer and renders it on the screen

--- src/main/java/renderer/Shader.java
+++ src/main/java/renderer/Shader.java
@@ -1,6 +1,10 @@
 package renderer;
 
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
+
 import java.io.IOException;
+import java.nio.FloatBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -135,6 +139,17 @@ public class Shader
     public void detach()
     {
         glUseProgram(0);
+    }
+
+
+    // Function that puts the Matrix4f into the buffer to be rendered to the screen
+    public void uploadMat4f(String varName, Matrix4f mat4)
+    {
+        int varLocation = glGetUniformLocation(shaderProgramID, varName);
+        FloatBuffer matBuffer = BufferUtils.createFloatBuffer(16); // capacity is 16 since its 4x4 matrix
+        mat4.get(matBuffer);
+        glUniformMatrix4fv(varLocation, false, matBuffer);
+
     }
 
 
