3f47f617a241fa8c59451237ce798e8a166d1dbe
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-05 13:53:22 +0100

    Update #9
    Create a list of GameObjects in the Scene.
    
    method start - For every game object call method start
    
    method addGameObjectToScene - Insert the given game object into the list of game objects in the scene.
    If the engine is already running means it will be added but not started, so
    we have to call it manually.

--- src/main/java/engine/Scene.java
+++ src/main/java/engine/Scene.java
@@ -1,10 +1,17 @@
 package engine;
 
+import ECS.GameObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Scene
 {
 
     // Only pass camera Class to inherited classes of Scene class.
     protected Camera camera;
+    private boolean b_isRunning = false;
+    protected List<GameObject> gameObjects = new ArrayList<>();
 
     public Scene()
     {
@@ -12,10 +19,27 @@ public abstract class Scene
     }
 
     public abstract void update(float dt);
+    public void init() {}
 
-    public void init()
+
+    public void start()
     {
-
+        for(GameObject go_obj : gameObjects)
+        {
+            go_obj.start();
+        }
+        b_isRunning = true;
     }
 
+    public void addGameObjectToScene(GameObject go_obj)
+    {
+        if(!b_isRunning) gameObjects.add(go_obj);
+
+        else
+        {
+            gameObjects.add(go_obj);
+            go_obj.start();
+        }
+
+    }
 }
