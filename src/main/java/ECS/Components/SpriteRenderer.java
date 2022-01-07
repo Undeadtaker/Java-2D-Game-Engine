bbaf70fca9e293272345ad02bc8ae4be3ad00a3c
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-07 18:36:43 +0100

    Update #12
    The sprite now hosts a variable that holds coordinates for the vertices and where they will be rendered.
    The new Constructor allows the sprite to be initiated with a Texture and not just a color.
    The function getTexCoords initiates these position coordinates which are hardcoded for now.

--- src/main/java/ECS/Components/SpriteRenderer.java
+++ src/main/java/ECS/Components/SpriteRenderer.java
@@ -1,7 +1,9 @@
 package ECS.Components;
 
 import ECS.Component;
+import org.joml.Vector2f;
 import org.joml.Vector4f;
+import renderer.Texture;
 
 public class SpriteRenderer extends Component
 {
@@ -9,10 +11,32 @@ public class SpriteRenderer extends Component
     // Variables
     private Vector4f color;
 
+    /*
+        Texture coordinates passed to the GPU have to have the following coordinates
+
+        (0, 1) -> bottom right
+        (0, 0) -> bottom left
+        (1, 1) -> top right
+        (1, 0) -> top left
+
+        That's why we need a Vector2f to store the coordinates of the texture
+    */
+    private Vector2f[] texCoords;
+    private Texture texture;
+
+
     // Constructor
     public SpriteRenderer(Vector4f color)
     {
         this.color = color;
+        this.texture = null;
+    }
+
+    // Constructor that will create a Sprite with a texture instead of just color
+    public SpriteRenderer(Texture texture)
+    {
+        this.texture = texture;
+        this.color = new Vector4f(1, 1, 1, 1); // white
     }
 
 
@@ -26,6 +50,24 @@ public class SpriteRenderer extends Component
     public Vector4f getColor()
     {
         return this.color;
+    }
+
+
+    public Texture getTexture()
+    {
+        return this.texture;
+    }
+
+    public Vector2f[] getTexCoords()
+    {
+        Vector2f[] texCoords = {
+                new Vector2f(1, 1),
+                new Vector2f(1, 0),
+                new Vector2f(0, 0),
+                new Vector2f(0, 1)
+        };
+
+        return texCoords;
     }
 
 }
