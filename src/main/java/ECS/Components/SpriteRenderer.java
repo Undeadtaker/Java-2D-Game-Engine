0586c7b603d29b8563abdf491f2db60c7378d205
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-09 18:46:50 +0100

    Update #14 - Implemented the concept of dirty flag
    Implemented a concept of dirty flag
    
    + In this SpriteRenderer class updated method start to create a new object called lastTransform which is a Transform object.
    This Transform calls the then overridden Object class method equals. It checks if the lastTransform equals the new one, if it is, we set the dirty flag to true.
    
    + In the method setSprite we set the dirty flag to true as well because a Sprite can change its index, hence rebuffering needed.
    
    +In the setColor method we change the method for the Sprite, hence rebuffering needed.
    
    + Added boolean isDirty method
    + Added void setClean method
    + added Vector4f getColor method
    + added Texture getTexture method

--- src/main/java/ECS/Components/SpriteRenderer.java
+++ src/main/java/ECS/Components/SpriteRenderer.java
@@ -1,6 +1,7 @@
 package ECS.Components;
 
 import ECS.Component;
+import engine.Transform;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
 import renderer.Texture;
@@ -11,6 +12,9 @@ public class SpriteRenderer extends Component
     // Variables
     private Vector4f color;
     private Sprite sprite;
+    boolean b_isDirty = false;
+
+    private Transform lastTransform;
 
 
     // Constructor
@@ -30,19 +34,51 @@ public class SpriteRenderer extends Component
 
     // Methods
     @Override
-    public void update(float dt) {}
+    public void start()
+    {
+        // Create a new Transform that is equal to the gameObject's current Transform,
+        // the SpriteRenderer extends from Component which means it inherits the
+        // gameObject object
+        this.lastTransform = gameObject.transform.copy();
+    }
 
     @Override
-    public void start() {}
+    public void update(float dt)
+    {
+        // Check if the lastTransform is equal to the new version of Transform's values
+        // by using the equals overriden method in Transform class
+        if(!this.lastTransform.equals(this.gameObject.transform)) {
+            this.gameObject.transform.copy(this.lastTransform);
+            this.b_isDirty = true;
+        }
+    }
 
-    public Vector4f getColor()
+    public void setSprite(Sprite sprite)
     {
-        return this.color;
+        this.sprite = sprite;
+
+        // Set isDirty to true if sprite has changed
+        this.b_isDirty = true;
     }
-    public Texture getTexture()
+
+    public void setColor(Vector4f color)
     {
-        return sprite.getTexture();
+        if(!this.color.equals(color))
+        {
+            this.color.set(color);
+
+            // Set isDirty to true if the color has changed
+            this.b_isDirty = true;
+        }
     }
+
+
+    public boolean isDirty() {return this.b_isDirty;}
+    public void setClean() {this.b_isDirty = false;}
+    public Vector4f getColor() {return this.color;}
+    public Texture getTexture() {return sprite.getTexture();}
     public Vector2f[] getTexCoords() {return sprite.getTexCoords();}
+
+
 
 }
