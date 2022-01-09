4b4c5315bdc467c8e110c775d96c5be19a6917f4
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-09 18:50:47 +0100

    Update #14 - Implemented the concept of dirty flag
    + Added public Transform copy method, this method returns a newly created Transform object with the current one's position and scale,
    
    + Added void copy method, this method passes the Transform object which will have its position and scale adjusted to the current Transform's position and scale.
    
    + Overridden method equals, returns check if the position and scale of the passed object is equal to the this objects position and scale.

--- src/main/java/engine/Transform.java
+++ src/main/java/engine/Transform.java
@@ -34,5 +34,28 @@ public class Transform
         this.scale = scale;
     }
 
+    public Transform copy()
+    {
+        return new Transform(new Vector2f(this.position), new Vector2f(this.scale));
+    }
+
+    // Copy to the new transform object the contents of this transform
+    public void copy(Transform obj_to_copy_values_to)
+    {
+        obj_to_copy_values_to.position.set(this.position);
+        obj_to_copy_values_to.scale.set(this.scale);
+    }
+
+
+    // Overriding the OBJECT method equals, check if this transform is the same as the passed one
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null) return false;
+        if (!(obj instanceof Transform)) return false;
+
+        Transform t = (Transform) obj;
+        return t.position.equals(this.position) && t.scale.equals(this.scale);
+    }
 
 }
