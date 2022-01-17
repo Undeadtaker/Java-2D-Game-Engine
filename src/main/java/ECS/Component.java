82e0b934e572e0b052412fc4b1a5a49f5a6c93db
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-17 23:44:12 +0100

    Update #19
    In this abstract we define the class updateComponentImgui, which gives a base default method for every subclass.
    
    We get the SUBCLASSE'S class type and fields. The line
    
    Class<?> Type = field.getTyoe();
    
    Is important. In it we guess which type the class is. Then for the simple types we can dynamically create a ImGui element that will be drawn on the screen.
    The good thing is that if we want to do anything custom for any specific non primitive class in the ImGui we can.

--- src/main/java/ECS/Component.java
+++ src/main/java/ECS/Component.java
@@ -1,11 +1,49 @@
 package ECS;
 
+import imgui.ImGui;
+
+import java.lang.reflect.Field;
+
 public abstract class Component
 {
     public transient GameObject gameObject = null;
 
     public abstract void update(float dt);
     public void start() {}
-    public void updateComponentImgui() {}
+
+
+    public void updateComponentImgui()
+    {
+        try
+        {
+            // Doesn't return the fields of the Component class, rather the fields of the
+            // class derived from the Component Class
+            Field[] fields_li = this.getClass().getDeclaredFields();
+            for(Field field : fields_li)
+            {
+                // We get all the information we need to dynamically display
+                // input for the basic derived components.
+                Class<?> Type = field.getType();
+                Object value = field.get(this);
+                String name = field.getName();
+
+                if(Type == int.class)
+                {
+                    int val = (int) value;
+                    int[] imInt = {val};
+
+                    if(ImGui.dragInt(name + ": ", imInt))
+                    {
+                        field.set(this, imInt[0]);
+                    }
+                }
+
+            }
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+    }
 
 }
