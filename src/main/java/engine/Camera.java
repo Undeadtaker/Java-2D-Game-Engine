9cc6160847491bee9db84aff88e22bbdd631f936
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-20 23:33:31 +0100

    Update #21
    Camera now has two new variables. These are the inverse of the projection matrix and the inverse of the view matrix. We initialize these matrices as empty matrices in the constructor.  We put getters for these matrices.

--- src/main/java/engine/Camera.java
+++ src/main/java/engine/Camera.java
@@ -6,7 +6,7 @@ import org.joml.Vector3f;
 
 public class Camera
 {
-    private final Matrix4f projectionMatrix, viewMatrix;
+    private final Matrix4f projectionMatrix, viewMatrix, inverseProjection, inverseView;
     public Vector2f position;
 
 
@@ -15,6 +15,8 @@ public class Camera
         this.position = pos;
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
+        this.inverseProjection = new Matrix4f();
+        this.inverseView = new Matrix4f();
         this.adjustProjection();
     }
 
@@ -22,6 +24,7 @@ public class Camera
     {
         projectionMatrix.identity();
         projectionMatrix.ortho(0.0f, 32.0f * 40.0f ,0.0f, 32.0f * 21.0f, 0.0f, 100.0f);
+        projectionMatrix.invert(inverseProjection);
     }
 
     public Matrix4f getViewMatrix()
@@ -33,13 +36,13 @@ public class Camera
         viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
                                             cameraFront.add(position.x, position.y, 0.0f),
                                             cameraUp);
+
+        this.viewMatrix.invert(inverseView);
         return this.viewMatrix;
     }
 
-    public Matrix4f getProjectionMatrix()
-    {
-        return this.projectionMatrix;
-    }
-
+    public Matrix4f getProjectionMatrix() {return this.projectionMatrix;}
+    public Matrix4f getInverseProjection() {return this.inverseProjection;}
+    public Matrix4f getInverseView() {return this.inverseView;}
 
 }
