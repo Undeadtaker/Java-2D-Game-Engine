d50994730723512011be543caae78bed94c92f9e
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-20 23:30:35 +0100

    Update #21
    Added width, height to the sprite. Now we can set the sprite width and height and see them on the screen according to normalized world coordinates.

--- src/main/java/ECS/Components/Sprite.java
+++ src/main/java/ECS/Components/Sprite.java
@@ -5,6 +5,8 @@ import renderer.Texture;
 
 public class Sprite
 {
+    private float width, height;
+
     // Variables
     private Texture texture = null;
     private Vector2f[] texCoords = new Vector2f[] {
@@ -20,4 +22,9 @@ public class Sprite
     // Getters
     public Texture getTexture(){return this.texture;}
     public Vector2f[] getTexCoords(){return this.texCoords;}
+    public float getWidth() {return width;}
+    public void setWidth(float width) {this.width = width;}
+    public float getHeight() {return height;}
+    public void setHeight(float height) {this.height = height;}
+    public int getTexId() {return texture == null ? -1 : texture.getTexID();}
 }
