14ead2d1bb8d153356904bc24941feca1a7afbcf
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-21 20:33:57 +0100

    Update #22
    + Added ECS/Components/MouseControls.java
    
    This class is a Component that reacts with the MouseListener class. It checks for gameObject that has been picked up by the mouse, adjusts its position, and places it into the world after the user has left clicked.

---
+++ src/main/java/ECS/Components/MouseControls.java
@@ -0,0 +1,42 @@
+/* This class is created as a helper for any
+   type of controls we want to do with our mose. */
+
+package ECS.Components;
+
+import ECS.Component;
+import ECS.GameObject;
+import engine.MouseListener;
+import engine.Window;
+
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
+
+public class MouseControls extends Component
+{
+    // Held object is the gameObject the mouse is holding
+    GameObject heldObject = null;
+
+    public void pickupObject(GameObject GObj) {
+        this.heldObject = GObj;
+
+        // After we hold the object, we want to make it visible to the user
+        Window.getScene().addGameObjectToScene(GObj);
+    }
+
+    @Override
+    public void update(float dt)
+    {
+        if(heldObject != null)
+        {
+            heldObject.transform.position.x = MouseListener.getOrthoX() - 16;
+            heldObject.transform.position.y = MouseListener.getOrthoY() - 16;
+
+            if(MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_LEFT))
+            {
+                this.place();
+            }
+        }
+    }
+
+
+    public void place() {this.heldObject = null;}
+}
