8f3c6ccb5037f5e36f9ee56892e9ab097c2b4ad6
Undeadtaker <61665341+Undeadtaker@users.noreply.github.com>
2022-01-29 22:32:13 +0100

    Update #27
    + Created new class java/editor/GameViewViewport
    
    This class is responsible for initializing the viewport for the Game where the level is being built.
    
    It calculates the largest size for the viewport as well as the centered position for the viewport which is used when rendering the size of the window on the display.
    
    In the texture changed from texture_min and texture_mag filter for openGL to be linear, which means they stretch beyond border.

--- src/main/java/renderer/Texture.java
+++ src/main/java/renderer/Texture.java
@@ -32,6 +32,9 @@ public class Texture
         this.texID = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, texID);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height,
                 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
 
